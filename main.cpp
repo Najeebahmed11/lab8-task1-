af5a57e26477d3f4d87467920b66eb97cef6c024
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2018-11-14 22:40:24 +0500

    Add files via upload
    I HAVE UPLODED MAIN FILE

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array[10][3];
+    for(int i=0;i<10;i++)
+    {
+        int k=0;
+        cout<<"enter student number"<<endl;
+        for(int j=0;j<3;j++)
+        {
+            cin>>array[i][j];
+            if (array[i][j]>100)
+            {
+                cout<<"it is invalid input,please enter a number between 0-100"<<endl;
+                cin>>array[i][j];
+            }
+                k=k+array[i][j];
+        }
+        k=k/3;
+        cout<<"this student average is "<<k<<endl<<endl;
+    }
+        for(int i=0;i<3;i++)
+            {
+                int l=0;
+                for (int j=0;j<10;j++)
+                    {
+                        l=l+array[i][j];
+                    }
+                l=l/10;
+                cout<<"this subject average is "<<l<<endl;
+            }
+    for(int i=0;i<10;i++)
+    {
+        for (int j=0;j<3;j++)
+        {
+            cout<<array[i][j];
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
